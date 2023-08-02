50fc9edcad76c9f2a14274868e23069c5c430a4d
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-08-02 17:31:45 +0300

    fix 2/5 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 const list = document.querySelector(".gallery");
 
 const markup = galleryItems
@@ -7,12 +6,11 @@ const markup = galleryItems
     ({ preview, original, description }) => `
     <li class="gallery__item">
     <a class="gallery__link" href="${original}">
-       <img class="gallery__image" src="${preview}" alt="${description}"title = '${description}'/>
+       <img class="gallery__image" src="${preview}" alt="${description}" title = '${description}'/>
     </a>
  </li>`
   )
   .join("");
 list.insertAdjacentHTML("beforeend", markup);
 
-let lightbox = new SimpleLightbox('.gallery a', {  });
-
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
